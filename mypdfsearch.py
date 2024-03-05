1f92bf7528c53371700dc49b4a44258254d05e98
Beatriz Brito <beatriz.debritolima1998@gmail.com>
2024-03-05 20:29:15 -0300

    pdf2docx

--- mypdfsearch.py
+++ mypdfsearch.py
@@ -2,9 +2,12 @@
 #03032024
 
 import sys
-import aspose.words as aw
+#import aspose.words as aw
+from pdf2docx import Converter
 
 file = sys.argv[1]
 
-pdf = aw.Document(file)
-pdf.save('file.docx')
+cv = Converter(file)
+cv.convert('./file.docx')
+# pdf = aw.Document(file)
+# pdf.save('file.docx')
